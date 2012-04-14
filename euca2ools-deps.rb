c60afe679f42dddf16b609f8a6b03ba49014f28a
Sander van Zoest <svanzoest@onerecovery.com>
2012-04-13 17:49:02 -0700

    cleanup the download name for the euca2ools source

--- euca2ools-deps.rb
+++ euca2ools-deps.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Euca2oolsDeps < Formula
-  url 'http://open.eucalyptus.com/sites/all/modules/pubdlcnt/pubdlcnt.php?file=http://eucalyptussoftware.com/downloads/releases/euca2ools-1.3.1-src-deps.tar.gz&nid=1346'
+  url 'http://open.eucalyptus.com/sites/all/modules/pubdlcnt/pubdlcnt.php?nid=1346&file=http://eucalyptussoftware.com/downloads/releases/euca2ools-1.3.1-src-deps.tar.gz'
   homepage 'http://open.eucalyptus.com/downloads'
   version '1.3.1'
   md5 'a33ee3a129fa9135d9896cc9fc5a621a'
