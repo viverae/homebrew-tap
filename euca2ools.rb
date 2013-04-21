93703881e090bc7c241f16e2fcc1e15ef8e98987
Sander van Zoest <sander@vanzoest.com>
2013-04-20 20:29:24 -0700

    update 2.1.3 and attempt new dependency based install method

--- euca2ools.rb
+++ euca2ools.rb
@@ -1,13 +1,36 @@
 require 'formula'
 
 class Euca2ools < Formula
-  url 'https://github.com/eucalyptus/euca2ools/archive/2.1.2.tar.gz'
-  homepage 'http://www.eucalyptus.com/download/euca2ools'
-  md5 '9a517e7bed703652a2264048e5a98588'
+ depends_on 'python'
+ depends_on 'boto' => :python
+ depends_on 'M2Crypto' => :python
 
-  def install
-    system "easy_install boto"
-    system "python setup.py install"
-    system "easy_install M2Crypto"
-  end
+ url 'https://github.com/eucalyptus/euca2ools/archive/2.1.3.tar.gz'
+ homepage 'http://www.eucalyptus.com/download/euca2ools'
+ md5 'a76c752e6e7a7eeab978ba69f14788b4'
+
+ def install
+   # In order to install into the Cellar, the dir must exist and be in the PYTHONPATH.
+   temp_site_packages = lib/which_python/'site-packages'
+   mkdir_p temp_site_packages
+   ENV['PYTHONPATH'] = temp_site_packages
+
+   args = [
+     "--no-user-cfg",
+     "--verbose",
+     "install",
+     "--force",
+     "--install-scripts=#{bin}",
+     "--install-lib=#{temp_site_packages}",
+     "--install-data=#{share}",
+     "--install-headers=#{include}",
+   ]
+
+   system "python", "-s", "setup.py", *args
+ end
+
+ def which_python
+   # Update this once we have something like [this](https://github.com/mxcl/homebrew/issues/11204)
+   "python" + `python -c 'import sys;print(sys.version[:3])'`.strip
+ end
 end
