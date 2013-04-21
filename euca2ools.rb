8ff1352dd55bb548e9d663cdba69d8f8fb3a6b30
Sander van Zoest <sander@vanzoest.com>
2013-04-20 22:29:48 -0700

    use pip (again)

--- euca2ools.rb
+++ euca2ools.rb
@@ -4,10 +4,9 @@ class Euca2ools < Formula
  depends_on 'python'
  # for M2Crypto, we are't using this because swig attempts to install after the pip dependency output
  # so naturally the pip install commands fail (as swig is not installed yet)
- #depends_on 'swig'
+ depends_on 'swig'
  #depends_on 'M2Crypto' => :python
  depends_on 'boto' => :python
- depends_on 'swig'
 
  url 'https://github.com/eucalyptus/euca2ools/archive/2.1.3.tar.gz'
  homepage 'http://www.eucalyptus.com/download/euca2ools'
@@ -15,8 +14,8 @@ class Euca2ools < Formula
 
  def install
    # unfortunately, the pip version depends on swig which doesn't run before the python dependency runs
-   # for some reason easy_install is able to find a binary for osx instead; which makes this much easier
-   system 'easy_install m2crypto'
+   # so this way it at least installs eventhough it isn't recommended
+   system 'pip install  m2crypto'
    # In order to install into the Cellar, the dir must exist and be in the PYTHONPATH.
    temp_site_packages = lib/which_python/'site-packages'
    mkdir_p temp_site_packages
