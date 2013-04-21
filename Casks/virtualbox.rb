b2200ae31b1618cf402bce259bb2029e39f65f2f
Sander van Zoest <sander@vanzoest.com>
2013-04-20 22:53:44 -0700

    test

--- Casks/virtualbox.rb
+++ Casks/virtualbox.rb
@@ -4,5 +4,5 @@ class Virtualbox < Cask
     version '4.2.12-84980'
     sha1 '9a5913f0b25c69a67b554caa9bebd1a4d72d3d00'
 
-    system "open #{self.class.destination_path}/VirtualBox.pkg"
+    system "open #{self.destination_path}/VirtualBox.pkg"
 end
